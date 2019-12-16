5a85c9da7013df8983f6cead19fa7d399be2d84f
woorui <rui1009479218@gmail.com>
2019-12-16 11:04:40 +0800

    update

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ func isDeLimited(b byte) bool {
 	return b == 10 || b == 32 || b == 9
 }
 
-func readArgs(maxArgs int, argsCh chan []string) {
+func readArgs(maxArgs int) (argsCh chan []string) {
+	defer close(argsCh)
 	reader := bufio.NewReader(os.Stdin)
 	var args []string
 	arg := ""
@@ -41,34 +42,24 @@ func readArgs(maxArgs int, argsCh chan []string) {
 			}
 		}
 	}
-	close(argsCh)
+	return argsCh
 }
 
-func buildCmd(cmdsCh chan CmdWithArgs, name string, args []string) {
-	cwa := CmdWithArgs{name: name, args: args}
-	// fmt.Println(cwa)
-	cmdsCh <- cwa
-}
-
-func buildGroupCmd(n int, maxProcs int, cmdsCh chan CmdWithArgs, cmdsGroupCh chan []CmdWithArgs) {
-	cmds := []CmdWithArgs{}
-	for i := 0; i < n; i++ {
-		cwa := <-cmdsCh
-		if len(cmds) == maxProcs {
-			cmdsGroupCh <- cmds
-			cmds = []CmdWithArgs{}
-
+func buildCmd(bin string, maxProcs int, argsCh chan []string) chan CmdWithArgs {
+	cmdsCh := make(chan CmdWithArgs, maxProcs)
+	defer close(cmdsCh)
+	go func() {
+		for args := range argsCh {
+			cwa := CmdWithArgs{name: bin, args: args}
+			cmdsCh <- cwa
 		}
-		cmds = append(cmds, cwa)
-	}
-	if len(cmds) != 0 {
-		cmdsGroupCh <- cmds
-	}
-	close(cmdsGroupCh)
+	}()
+	return cmdsCh
 }
 
-func execGroupCmd(wg *sync.WaitGroup, cmdsGroup []CmdWithArgs) {
-	for _, c := range cmdsGroup {
+func execCmd(cmdsCh chan CmdWithArgs) {
+	var wg sync.WaitGroup
+	for c := range cmdsCh {
 		wg.Add(1)
 		go func(c CmdWithArgs) {
 			cmd := exec.Command(c.name, c.args...)
@@ -78,6 +69,7 @@ func execGroupCmd(wg *sync.WaitGroup, cmdsGroup []CmdWithArgs) {
 			wg.Done()
 		}(c)
 	}
+	wg.Wait()
 }
 
 func main() {
@@ -93,28 +85,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	argsCh := make(chan []string)
-
 	// Read From stdin
-	go readArgs(*maxArgs, argsCh)
-
-	cmdsCh := make(chan CmdWithArgs, 2)
-	cmdsGroupCh := make(chan []CmdWithArgs)
+	argsCh := readArgs(*maxArgs)
 
 	// Build name and args used by command
-	n := 0
-	for args := range argsCh {
-		n++
-		go buildCmd(cmdsCh, *bin, args)
-	}
-
-	// Group the command for execute asynchronously
-	go buildGroupCmd(n, *maxProcs, cmdsCh, cmdsGroupCh)
+	cmdsCh := buildCmd(*bin, *maxProcs, argsCh)
 
 	// execute the command Grouped
-	var wg sync.WaitGroup
-	for cg := range cmdsGroupCh {
-		execGroupCmd(&wg, cg)
-	}
-	wg.Wait()
+	execCmd(cmdsCh)
 }
